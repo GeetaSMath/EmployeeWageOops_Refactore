08edebb123e60a7745a046a5bd5132aa1d341736
GeetaSMath <geetasmath@gmail.com>
2021-10-22 13:25:54 -0600

    Added: Calculating Employee Wage in Switch Case

--- src/com/bridgelabz/EmployeeWageOopsProblem.java
+++ src/com/bridgelabz/EmployeeWageOopsProblem.java
@@ -2,20 +2,23 @@ package com.bridgelabz;
 
 public class EmployeeWageOopsProblem {
 
-
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Computation Problem");
 
-        int IS_FULL_TIME=1;
+        final int IS_FULL_TIME=1;
+        final int IS_PART_TIME=2;
         int WAGE_PER_HOUR=20;
         int empHrs=0;
         int empWage=0;
 
-        int empCheck = (int) (Math.floor(Math.random()*10)%2);
+        int empCheck = (int) (Math.floor(Math.random()*10)%3);
 
         if(empCheck == IS_FULL_TIME) {
             System.out.println("Employee is present");
             empHrs=8;
+        }else if(empCheck == IS_PART_TIME) {
+            System.out.println("Employee is present but part time");
+            empHrs=4;
         }
         else {
             System.out.println("Employee is absent");
@@ -25,7 +28,18 @@ public class EmployeeWageOopsProblem {
         int dailyWage = empHrs * WAGE_PER_HOUR;
         System.out.println("Daily Wage of Employee : "+dailyWage);
 
+        switch(empCheck) {
+            case IS_FULL_TIME: empHrs=8;
+                break;
+            case IS_PART_TIME: empHrs=4;
+                break;
+            default: empHrs=0;
+
+        }
+        int dailyWageSwitch = empHrs * WAGE_PER_HOUR;
+        System.out.println("Daily Wage of Employee using Switch: "+dailyWageSwitch);
+
 
     }
 
-}
+    }
